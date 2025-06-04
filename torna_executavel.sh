e875883557bdc96bb31eb5c3d4e7a44320cd3fef
Wilian <75041193+wilianprofirio@users.noreply.github.com>
2025-06-04 11:32:54 -0300

    Create torna_executavel.sh

---
+++ torna_executavel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#####	NOME:				torna_executavel.sh
+#####	VERSÃO:				1.2
+#####	DESCRIÇÃO:			Implementação de rotina para tornar aqruivos executaveis
+#####	DATA DA CRIAÇÃO:	18/03/2025
+##### ATUALIZADO EM: 18/04/2025
+#####	ESCRITO POR:		Wilian Profirio da Silva
+#####	E-MAIL:				ntswilian@gmail.com
+#####	DISTRO:				Ubuntu GNU/Linux 22.04
+#####	LICENÇA:			MIT license
+#####	PROJETO:			https://github.com/wilianprofirio/servidor-file-samba4
+
+## chmod +x novo_script
+
+cd /home/servidor/backup/diario/servidor-file-samba4
+chmod +x rotina_backup_diario.sh delete_file_olds.sh rotina_backup.sh
